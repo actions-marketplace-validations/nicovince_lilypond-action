d7ecf1472eb1a48f90e27ed485d8294d0ce1b331
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2023-02-03 16:40:13 +0100

    action: support make based build
    Adds input option for action to specify how the build is managed.
    'simple' mode will call manually the lilypond tool,
    'make' will use a makefile provided where the action is run.

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ ENV TZ=Europe/Paris
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone && \
     apt-get update && apt-get install -y \
     lilypond \
+    make \
     && rm -rf /var/lib/apt/lists/* \
     && mkdir -p /data/src
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 ARGS="$@"
 OUTPUT=""
-echo "Lilypond list: ${ARGS}"
-for f in ${ARGS}; do
-    echo "Processing ${f}"
-    if [ ! -f ${f} ]; then
-        echo "Error: ${f} does not exists"
+
+function simple_mode()
+{
+    echo "Lilypond list: ${INPUT_LY_FILES}"
+    for f in ${INPUT_LY_FILES}; do
+        echo "Processing ${f}"
+        if [ ! -f ${f} ]; then
+            echo "Error: ${f} does not exists"
+            exit 1
+        fi
+
+        lilypond -o $(dirname ${f}) ${f}
+        if [ $? -ne 0 ]; then
+            echo "Error: Compilation failure on ${f}"
+        fi
+
+        OUTPUT="${OUTPUT}${f%%ly}pdf "
+    done
+
+    echo "pdfs=${OUTPUT}" >> ${GITHUB_OUTPUT}
+}
+
+function make_mode()
+{
+    make -C $(pwd)/${INPUT_MAKE_DIR} ${INPUT_MAKE_TARGET}
+    if [ $? -ne 0 ]; then
+        echo "Make based build failed"
         exit 1
     fi
+    PDFS=$(find . -name "*.pdf")
+    echo "pdfs=${PDFS}" >> ${GITHUB_OUTPUT}
+}
 
-    lilypond -o $(dirname ${f}) ${f}
-    if [ $? -ne 0 ]; then
-        echo "Error: Compilation failure on ${f}"
-    fi
-
-    OUTPUT="${OUTPUT}${f%%ly}pdf "
-done
-
-echo "pdfs=${OUTPUT}" >> ${GITHUB_OUTPUT}
+if [ ${INPUT_BUILD_MODE} = "simple" ]; then
+    simple_mode
+elif [ ${INPUT_BUILD_MODE} = "make" ]; then
+    make_mode
+fi
